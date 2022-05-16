bd75c8581e16492f06a8134bbdc34630a7956290
Jayden Jin <webstar2222@gmail.com>
2022-05-16 02:25:43 -0600

    initial commit

--- js/main.js
+++ js/main.js
@@ -121,7 +121,7 @@ $(function () {
     },
   });
 
-  bar.animate(1);
+  bar.animate(0.85);
 
   var bar = new ProgressBar.Circle(circleprog2, {
     strokeWidth: 7,
@@ -139,7 +139,7 @@ $(function () {
     },
   });
 
-  bar.animate(0.9);
+  bar.animate(0.8);
 
   var bar = new ProgressBar.Circle(circleprog3, {
     strokeWidth: 7,
@@ -174,7 +174,7 @@ $(function () {
     },
   });
 
-  bar.animate(1);
+  bar.animate(0.8);
 
   var bar = new ProgressBar.Line(lineprog2, {
     strokeWidth: 1.72,
@@ -191,7 +191,7 @@ $(function () {
     },
   });
 
-  bar.animate(1);
+  bar.animate(0.9);
 
   var bar = new ProgressBar.Line(lineprog3, {
     strokeWidth: 1.72,
@@ -208,7 +208,7 @@ $(function () {
     },
   });
 
-  bar.animate(0.8);
+  bar.animate(0.6);
 
   var bar = new ProgressBar.Line(lineprog4, {
     strokeWidth: 1.72,
@@ -225,7 +225,7 @@ $(function () {
     },
   });
 
-  bar.animate(1);
+  bar.animate(0.75);
 
   var bar = new ProgressBar.Line(lineprog5, {
     strokeWidth: 1.72,
